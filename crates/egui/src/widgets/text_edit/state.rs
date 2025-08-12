59f836899a9ae184a487db0b8958e9fa7ca0b0d2
RndUsr123 <150948884+RndUsr123@users.noreply.github.com>
2025-08-12 10:27:55 +0000

    Fixes sense issues in TextEdit when vertical alignment is used (#7436)
    <!--
    Please read the "Making a PR" section of
    [`CONTRIBUTING.md`](https://github.com/emilk/egui/blob/main/CONTRIBUTING.md)
    before opening a Pull Request!
    
    * Keep your PR:s small and focused.
    * The PR title is what ends up in the changelog, so make it descriptive!
    * If applicable, add a screenshot or gif.
    * If it is a non-trivial addition, consider adding a demo for it to
    `egui_demo_lib`, or a new example.
    * Do NOT open PR:s from your `master` branch, as that makes it hard for
    maintainers to test and add commits to your PR.
    * Remember to run `cargo fmt` and `cargo clippy`.
    * Open the PR as a draft until you have self-reviewed it and run
    `./scripts/check.sh`.
    * When you have addressed a PR comment, mark it as resolved.
    
    Please be patient! I will review your PR, but my time is limited!
    -->
    
    * Closes <https://github.com/emilk/egui/issues/7433>
    * [ ] I have followed the instructions in the PR template
    
    I'm running a rustup-less rust install on Windows, so I don't have
    `clippy` nor `fmt` and can't run the .sh script.
    It's very little code and I manually tested this, so hopefully that's
    ok...
    
    Let me know if the comment in `state.rs` needs to be updated or the
    `text_offset` name isn't clear enough.
    
    ---------
    
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- crates/egui/src/widgets/text_edit/state.rs
+++ crates/egui/src/widgets/text_edit/state.rs
@@ -4,7 +4,7 @@ use alloc::sync::Arc;
 use crate::mutex::Mutex;
 
 use crate::{
-    Context, Id,
+    Context, Id, Vec2,
     text_selection::{CCursorRange, TextCursorState},
 };
 
@@ -50,9 +50,10 @@ pub struct TextEditState {
     #[cfg_attr(feature = "serde", serde(skip))]
     pub(crate) ime_cursor_range: CCursorRange,
 
-    // Visual offset when editing singleline text bigger than the width.
+    // Text offset within the widget area.
+    // Used for sensing and singleline text clipping.
     #[cfg_attr(feature = "serde", serde(skip))]
-    pub(crate) singleline_offset: f32,
+    pub(crate) text_offset: Vec2,
 
     /// When did the user last press a key or click on the `TextEdit`.
     /// Used to pause the cursor animation when typing.
