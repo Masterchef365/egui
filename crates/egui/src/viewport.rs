fc089aa453b520e418c32dc0652764c81164d420
Luke M <44519853+lkdm@users.noreply.github.com>
2025-08-12 20:35:45 +1000

    Document platform compatibility on `viewport::WindowLevel` and dependents (#7432)
    Documents platform compatibility on the WindowLevel struct and some of
    the methods that are dependent on it. Provides a link to
    `winit::window::WindowLevel` documentation section that provides a list
    of which platforms are supported.
    
    * Closes [Issue 7419](https://github.com/emilk/egui/issues/7419)
    * [x] I have followed the instructions in the PR template
    
    ---------
    
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- crates/egui/src/viewport.rs
+++ crates/egui/src/viewport.rs
@@ -634,6 +634,8 @@ impl ViewportBuilder {
     }
 
     /// Control if window is always-on-top, always-on-bottom, or neither.
+    ///
+    /// For platform compatibility see [`crate::viewport::WindowLevel`] documentation
     #[inline]
     pub fn with_window_level(mut self, level: WindowLevel) -> Self {
         self.window_level = Some(level);
@@ -641,6 +643,8 @@ impl ViewportBuilder {
     }
 
     /// This window is always on top
+    ///
+    /// For platform compatibility see [`crate::viewport::WindowLevel`] documentation
     #[inline]
     pub fn with_always_on_top(self) -> Self {
         self.with_window_level(WindowLevel::AlwaysOnTop)
@@ -902,6 +906,7 @@ impl ViewportBuilder {
     }
 }
 
+/// For winit platform compatibility, see [`winit::WindowLevel` documentation](https://docs.rs/winit/latest/winit/window/enum.WindowLevel.html#platform-specific)
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub enum WindowLevel {
