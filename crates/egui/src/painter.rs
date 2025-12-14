666361e055319210c55c200e9b4927aafdecb9f1
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-12-14 16:41:13 +0100

    Rename `Context::style` to `global_style`; avoid confusion w/ `Ui::style` (#7772)
    This is important after
    * https://github.com/emilk/egui/pull/7770

--- crates/egui/src/painter.rs
+++ crates/egui/src/painter.rs
@@ -323,8 +323,8 @@ impl Painter {
         );
     }
 
-    pub fn error(&self, pos: Pos2, text: impl core::fmt::Display) -> Rect {
-        let color = self.ctx.style().visuals.error_fg_color;
+    pub fn error(&self, pos: Pos2, text: impl std::fmt::Display) -> Rect {
+        let color = self.ctx.global_style().visuals.error_fg_color;
         self.debug_text(pos, Align2::LEFT_TOP, color, format!("🔥 {text}"))
     }
 
