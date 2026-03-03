b579fdf516bbcdeb3cec00768ea3de2af3bd894f
valadaptive <79560998+valadaptive@users.noreply.github.com>
2026-03-03 16:58:42 -0500

    Add font variations API (#7859)
    <!--
    Please read the "Making a PR" section of
    [`CONTRIBUTING.md`](https://github.com/emilk/egui/blob/main/CONTRIBUTING.md)
    before opening a Pull Request!
    
    * Keep your PR:s small and focused.
    * The PR title is what ends up in the changelog, so make it descriptive!
    * If applicable, add a screenshot or gif.
    * If it is a non-trivial addition, consider adding a demo for it to
    `egui_demo_lib`, or a new example.
    * Do NOT open PR:s from your `master` branch, as that makes it hard for
    maintainers to test and add commits to your PR.
    * Remember to run `cargo fmt` and `cargo clippy`.
    * Open the PR as a draft until you have self-reviewed it and run
    `./scripts/check.sh`.
    * When you have addressed a PR comment, mark it as resolved.
    
    Please be patient! I will review your PR, but my time is limited!
    -->
    
    * Closes N/A
    * [x] I have followed the instructions in the PR template
    
    This was mostly from last month, but I never got around to submitting
    it.
    
    This PR adds font variation coordinates to the `TextFormat` struct, and
    uses them when rendering text. The coordinates are stored in a
    `SmallVec`; I've chosen to store up to 2 inline, which makes it take up
    24 bytes (the minimum possible for a `SmallVec`). The variation axis
    tags are stored as the `font_types::Tag` type, which I've chosen to
    re-export from `epaint::text`.
    
    The variation coordinates are resolved to a `skrifa::Location` during
    font rendering/scaling, and are cached in the same way as all the other
    scaled metrics. I've renamed the `ScaledMetrics` struct to
    `StyledMetrics`, since it now also contains the resolved variation
    coordinates. I haven't benchmarked the performance of text layout with
    variation coordinates, but the existing text layout performance is
    unchanged.
    
    I've replaced the API for manually overriding a font's weight
    (https://github.com/emilk/egui/pull/7790) with an API for manually
    overriding any variation coordinates via `FontTweak`. This should
    support the same use case as #7790 while being substantially more
    flexible.
    
    I have *not* yet added any higher-level API for mapping style attributes
    (weight, width, slant, etc) to variation coordinates or to different
    font faces within a single family. That's a pretty huge can of worms,
    and it'd involve rethinking the split between `FontId` and `TextFormat`
    (and whether `FontId` is so big that we should provide a way to reuse
    it). This API is intentionally pretty low-level for now.
    
    Likewise, I've intentionally not used variation coordinates when
    computing a font's row height. I can't think of any fonts that change
    their vertical metrics depending on variation axes, so this should be
    fine for now.
    
    ---------
    
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- crates/egui/src/widget_text.rs
+++ crates/egui/src/widget_text.rs
@@ -1,10 +1,10 @@
 use alloc::borrow::ToOwned;
 use alloc::boxed::Box;
 use emath::GuiRounding as _;
-use epaint::text::TextFormat;
 use core::fmt::Formatter;
 use alloc::{borrow::Cow, sync::Arc};
 use alloc::string::{String, ToString};
+use epaint::text::{IntoTag, TextFormat, VariationCoords};
 
 use crate::{
     Align, Color32, FontFamily, FontSelection, Galley, Style, TextStyle, TextWrapMode, Ui, Visuals,
@@ -37,6 +37,7 @@ pub struct RichText {
     background_color: Color32,
     expand_bg: f32,
     text_color: Option<Color32>,
+    coords: VariationCoords,
     code: bool,
     strong: bool,
     weak: bool,
@@ -58,6 +59,7 @@ impl Default for RichText {
             background_color: Default::default(),
             expand_bg: 1.0,
             text_color: Default::default(),
+            coords: Default::default(),
             code: Default::default(),
             strong: Default::default(),
             weak: Default::default(),
@@ -196,6 +198,23 @@ impl RichText {
         let crate::FontId { size, family } = font_id;
         self.size = Some(size);
         self.family = Some(family);
+        self
+    }
+
+    /// Add a variation coordinate.
+    #[inline]
+    pub fn variation(mut self, tag: impl IntoTag, coord: f32) -> Self {
+        self.coords.push(tag, coord);
+        self
+    }
+
+    /// Override the variation coordinates completely.
+    #[inline]
+    pub fn variations<T: IntoTag>(
+        mut self,
+        variations: impl IntoIterator<Item = (T, f32)>,
+    ) -> Self {
+        self.coords = VariationCoords::new(variations);
         self
     }
 
@@ -394,6 +413,7 @@ impl RichText {
             background_color,
             expand_bg,
             text_color: _, // already used by `get_text_color`
+            coords,
             code,
             strong: _, // already used by `get_text_color`
             weak: _,   // already used by `get_text_color`
@@ -452,6 +472,7 @@ impl RichText {
                 line_height,
                 color: text_color,
                 background: background_color,
+                coords,
                 italics,
                 underline,
                 strikethrough,
